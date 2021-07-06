e79ebafefc63c3fe1112ac4ced73f34cc0174e46
nes <nes0510@gmail.com>
2021-07-06 23:37:13 +0900

    숙제 파일

---
+++ awtpractice/src/com/sen/awtpractice/Customer.java
@@ -0,0 +1,68 @@
+package com.sen.awtpractice;
+
+public class Customer {
+	   private String name;
+	   private String jumin;
+	   private String tel;
+	   private boolean gender;
+	   private String hobby;
+	   
+	   public Customer() {
+	      
+	   }
+
+	   public Customer(String name, String jumin, String tel, boolean gender, String hobby) {
+	      super();
+	      this.name = name;
+	      this.jumin = jumin;
+	      this.tel = tel;
+	      this.gender = gender;
+	      this.hobby = hobby;
+	   }
+
+	   public String getName() {
+	      return name;
+	   }
+
+	   public void setName(String name) {
+	      this.name = name;
+	   }
+
+	   public String getJumin() {
+	      return jumin;
+	   }
+
+	   public void setJumin(String jumin) {
+	      this.jumin = jumin;
+	   }
+
+	   public String getTel() {
+	      return tel;
+	   }
+
+	   public void setTel(String tel) {
+	      this.tel = tel;
+	   }
+
+	   public boolean isGender() {
+	      return gender;
+	   }
+
+	   public void setGender(boolean gender) {
+	      this.gender = gender;
+	   }
+
+	   public String getHobby() {
+	      return hobby;
+	   }
+
+	   public void setHobby(String hobby) {
+	      this.hobby = hobby;
+	   }
+
+	   @Override
+	   public String toString() {
+	      return name + " : " + (isGender() ? "남성" : "여성");
+	   }
+	   
+	}
