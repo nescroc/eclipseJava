8e401aa1b7957bce97a159b52495e07d0b86d7f4
NIS <nes0510@gmail.com>
2021-07-17 14:25:48 +0900

    TestMain

--- javaFxEx/src/com/sen/javaFx/TestMain.java
+++ javaFxEx/src/com/sen/javaFx/TestMain.java
@@ -5,7 +5,9 @@ import javafx.application.Application;
 public class TestMain {
 
 	public static void main(String[] args) {
+		/*window 화면 호출*/
 		Application.launch(MainView.class);
+		//호출방법3
 	}
 
 }
